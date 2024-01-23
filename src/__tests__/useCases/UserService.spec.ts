654d8fb3f79e4df6d51847f8a207fe002f3a2675
Thigo <thi.it.solutions@gmail.com>
2024-01-23 00:47:00 -0300

    chore(UserService): added test cases

--- src/__tests__/useCases/UserService.spec.ts
+++ src/__tests__/useCases/UserService.spec.ts
@@ -105,6 +105,67 @@ jest.mock('../../repositories/UserRepository', () => {
         return Promise.resolve(user || null)
       },
     ),
+    getUser: jest.fn((id: string): Promise<UserTypes | null> => {
+      if (id === '1') {
+        return Promise.resolve({
+          id: '1',
+          username: 'John',
+          name: 'John Doe',
+          email: 'john@example.com',
+          phone: '55 555-5555',
+          password: '123',
+          role: 'CLIENT',
+          isAdmin: false,
+          address: {
+            street: 'St. 15aver',
+            number: '23',
+            city: 'San Francisco',
+            country: 'United States',
+            zipCode: '29800000',
+          },
+        })
+      } else {
+        return Promise.resolve(null)
+      }
+    }),
+    getUsers: jest.fn(() => Promise.resolve(mockUser)),
+    updateUser: jest.fn(
+      (
+        id: string,
+        name: string,
+        username: string,
+        email: string,
+        phone: string,
+        password: string,
+        _isAdmin: boolean,
+        _role: UserRole,
+        _address: {
+          city: string
+          country: string
+          street: string
+          zipCode: string
+          number: string
+        },
+      ): Promise<UserTypes> => {
+        return Promise.resolve({
+          id: '123',
+          name: 'Jane Doe',
+          username: 'JaneDoe',
+          email: 'jane@example',
+          phone: '550223938232',
+          password: '123456',
+          role: 'ADMIN',
+          isAdmin: false,
+          address: {
+            city: 'San Francisco',
+            country: 'US',
+            street: 'San Francisco',
+            zipCode: '123',
+            number: '123',
+          },
+        })
+      },
+    ),
   }
 })
 
@@ -161,9 +222,52 @@ describe('UserRepository', () => {
     expect(user?.password).toBe(userData.password)
   })
 
-  it('should return a user by Id', () => {})
+  it('should return a user by Id', async () => {
+    const userId = '1'
+    const user = await userService.findUserById(userId)
 
-  it('should return all Users', () => {})
+    expect(mockedUserRepository.getUser).toHaveBeenCalledWith(userId)
+    expect(user?.id).toBe(userId)
+  })
 
-  it('should allow update user by Id', () => {})
+  it('should return null when the user id not fount by Id', async () => {
+    const userId = 'not-found'
+    const user = await userService.findUserById(userId)
+
+    expect(mockedUserRepository.getUser).toHaveBeenCalledWith(userId)
+    expect(user).toBeNull()
+  })
+
+  it('should return all Users', async () => {
+    const user = await userService.findAllUsers()
+
+    expect(mockedUserRepository.getUsers).toHaveBeenCalled()
+    expect(user).toEqual(mockUser)
+  })
+
+  it('should allow update user by Id', async () => {
+    const id = '123'
+    const name = 'Jane Doe'
+    const username = 'JaneDoe'
+    const phone = '550223938232'
+
+    const updateUser = await userService.updateUserById(
+      id,
+      name,
+      username,
+      phone,
+    )
+
+    expect(mockedUserRepository.updateUser).toHaveBeenCalledWith(
+      id,
+      name,
+      username,
+      phone,
+    )
+
+    expect(updateUser.id).toBe(id)
+    expect(updateUser.name).toBe(name)
+    expect(updateUser.phone).toBe(phone)
+    expect(updateUser.username).toBe(username)
+  })
 })
