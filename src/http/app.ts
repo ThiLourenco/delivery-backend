5ee25d819e3f5af0a09acbcf81525dec32d673c6
Thigo <thi.it.solutions@gmail.com>
2024-02-12 23:06:32 -0300

    feat(): implmenting security with cors and helmet

--- src/http/app.ts
+++ src/http/app.ts
@@ -1,10 +1,21 @@
 import express, { NextFunction, Request, Response } from 'express'
+import cors from 'cors'
+import helmet from 'helmet'
 import { router } from './routes'
 import { AppError } from '../errors/AppError'
 
 const app = express()
+app.use(helmet())
 app.use(express.json())
 app.use(router)
+app.use(cors())
+
+const corsOptions = {
+  origin: 'http://localhost:3000', // (https://your-client-app.com)
+  optionsSuccessStatus: 200,
+}
+
+app.use(cors(corsOptions))
 
 app.use((req, res, next) => {
   return res.status(404).json({ message: 'Not Found' })
